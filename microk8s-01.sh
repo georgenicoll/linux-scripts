0ea6873dbfd73c802f955fd0836b466a669a6e75
George Nicoll <george.nicoll@gmail.com>
2020-10-27 23:20:31 +0000

    Just use sudo

--- microk8s-01.sh
+++ microk8s-01.sh
@@ -5,16 +5,15 @@ sudo snap install microk8s --classic --channel=latest
 echo "--> Setup groups"
 sudo usermod -a -G microk8s $USER
 sudo chown -f -R $USER ~/.kube
-su - $USER
 
 echo "--> Waiting for microk8s to be ready"
-microk8s status --wait-ready
+sudo microk8s status --wait-ready
 
 echo "--> Alias kubectl to 'microk8s kubectl'"
 echo "alias kubectl='microk8s kubectl'" >> ~/.bash_aliases
 
 echo "--> Adding add-ons dns and storage"
-microk8s enable dns storage
+sudo microk8s enable dns storage
 
 echo "<-- Now move to clustering (re-login to pick up groups + alias)"
 

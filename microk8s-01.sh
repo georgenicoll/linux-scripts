e3309ad66da801379c81c88954be9f754ccfb2b5
George Nicoll <george.nicoll@gmail.com>
2020-10-27 23:04:41 +0000

    Script to setup microk8s basics

---
+++ microk8s-01.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+echo "--> Install microk8s"
+sudo snap install microk8s --classic --channel=latest
+
+echo "--> Setup groups"
+sudo usermod -a -G microk8s $USER
+sudo chown -f -R $USER ~/.kube
+
+echo "--> Waiting for microk8s to be ready"
+microk8s status --wait-ready
+
+echo "--> Alias kubectl to 'microk8s kubectl'"
+echo "alias kubectl='microk8s kubectl'" >> ~/.bash_aliases
+
+echo "--> Adding add-ons dns and storage"
+microk8s enable dns storage
+
+echo "<-- Now move to clustering (re-login to pick up groups + alias)"
+
